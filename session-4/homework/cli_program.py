ccfc3cb020253139817e5b45a74cbadc9619ae79
Jens Egholm <jensegholm@protonmail.com>
2019-07-08 15:06:50 +0200

    Corrected homework

--- session-4/homework/cli_program.py
+++ session-4/homework/cli_program.py
@@ -3,12 +3,7 @@ Adapted from: https://wiki.python.org/moin/Powerful%20Python%20One-Liners
 """
 import sys
 
-
-if __name__ == '__main__':
-    # Call me from the CLI for example with:
-    # echo "My cat looks weirdly. The neighbours cat too..." | python cli_program.py cat dog
-    pattern = sys.argv[1]
-    substitution = sys.argv[2]
-
-    for line in sys.stdin:
-        sys.stdout.write(line.replace(pattern, substitution))
+pattern = sys.argv[1]
+substitution = sys.argv[2]
+line = 'My cat looks weirdly. The neighbours cat too...'
+print(line.replace(pattern, substitution))
