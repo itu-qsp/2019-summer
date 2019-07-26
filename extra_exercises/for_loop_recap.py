e08a387b0d38b72b9e9e47571edf3e450e4eaf8f
Viktor torp Thomsen <viktortorpthomsen@Viktors-MacBook-Pro.local>
2019-07-26 11:54:26 +0200

    Some extra for loop exercises

---
+++ extra_exercises/for_loop_recap.py
@@ -0,0 +1,47 @@
+# Loop recap exercises
+
+# Some lists to loop over
+character = ['a','b','d','e','f']
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+# Create for loop, which prints all elements from character
+
+
+
+# Create for loop, which prints all elements from character with their index
+
+
+# Create a for loop which increments all elements from numbers by 1
+
+
+# Create a for loop which increments all elements from numbers by their index value
+
+
+# Loop over the list numbers and print all numbers less than 5
+
+
+# Loop over the list numbers and print all the even numbers
+
+
+# Loop over the list numbers and print all the even numbers, between 4 and 10.
+
+
+# loop over characters and muplitply it with some random numer, and add it to a new list
+
+
+# Write a Python program to construct the following pattern, using a nested for loop.
+# *
+# * *
+# * * *
+# * * * *
+# Hint use a range, which starts at 0 and ends at 5
+
+
+# Write a Python program to construct the following pattern, using a nested for loop.
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# Hint use a range, which starts at 5 and ends at 0
