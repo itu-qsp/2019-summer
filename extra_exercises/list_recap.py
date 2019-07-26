c372e9fe20f466b11189d2b7e17382351fa5c55d
Viktor torp Thomsen <viktortorpthomsen@Viktors-MacBook-Pro.local>
2019-07-26 10:20:29 +0200

    Here are some extra list exercises

---
+++ extra_exercises/list_recap.py
@@ -0,0 +1,63 @@
+# Inspired by the exercises from Automate the boring stuff Chapter 4.
+
+
+
+# Create a list called animals, containing the following strings: 'cat', 'bat', 'rat', 'elephant'
+
+
+# print the first element from animals
+
+
+# print the animal "rat"
+
+
+# print all of the animals
+
+
+
+# What happens if we use the index 100?
+
+
+
+# Can a list contain the following [1,2,3,"a","b","c",[1,2,3]]?
+
+
+# Create a new list named animals_with_weights containing the following two list: ['dog', 'bird', 'snake', 'fish'], [10, 20, 30, 40]
+
+
+# Create a new varibale called animal_names containing all of the animal names, from animals_with_weights
+
+
+# Create a new varibale called animal_weights containing all of the animal weights, from animals_with_weights
+
+
+# What will the following print?
+# print(animal_names[-1])
+
+# print(animal_weights[-2])
+
+# print(animals[1:2])
+
+# print(animals[1:4])
+
+# print(animals[::2])
+
+# print(animals[1::2])
+
+
+# What is the lenght of animals_with_weights
+
+
+# What is the lenght of animals_names
+
+
+# Try to create the following list  ['dog', 'bird', 'snake', 'fish', 10, 20, 30, 40], by using animal_names and animal_weights
+
+
+# create the following list ['dog', 'bird', 'snake', 'fish','dog', 'bird', 'snake', 'fish','dog', 'bird', 'snake', 'fish'], by using animal_names
+
+
+# Remove "bird" from animal_names
+
+
+# Remove the last item from animal_names
